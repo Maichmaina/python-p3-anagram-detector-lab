513ed710c5c58acc13a5fc0aae7914835a85ec55
Lennis <lennismaina75@gmail.com>
2024-06-05 08:18:05 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        matching = list()
+        sorted_word = sorted(self.word.lower())
+        for choice in anagrams:
+            if sorted(choice.lower()) == sorted_word and choice.lower() != self.word.lower():
+                matching.append(choice)
+        return matching
